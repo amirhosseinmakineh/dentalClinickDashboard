8097e6cfd68c3d8ed649ff10fa1e7648f2838207
amirhosseinmakineh <75474193+amirhosseinmakineh@users.noreply.github.com>
2026-06-02 01:40:07 -0700

    Reset app to Angular starter layout

--- src/app/components/dashboard-header/dashboard-header.component.ts
+++ src/app/components/dashboard-header/dashboard-header.component.ts
@@ -1,18 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-import { DecodedUser } from '../../models/auth.models';
 
 @Component({
   selector: 'app-dashboard-header',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './dashboard-header.component.html',
   styleUrl: './dashboard-header.component.scss'
 })
 export class DashboardHeaderComponent {
-  @Input({ required: true }) title = '';
-  @Input({ required: true }) subtitle = '';
-  @Input() user: DecodedUser | null = null;
+  @Input() title = 'Angular 20 Starter';
+  @Input() subtitle = 'Default Angular application shell with layout placeholders only.';
   @Output() toggleSidebar = new EventEmitter<void>();
-  @Output() logout = new EventEmitter<void>();
 }
